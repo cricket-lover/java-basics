a8cd9ea481cf3f131031204f8fd91689baec0a54
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-22 10:57:58 +0530

    added Hcf class which calculates HCF of two numbers

--- Factorial.java
+++ Factorial.java
@@ -1,20 +1,12 @@
 public class Factorial{
   public static void main(String [] args){
-  int num = 0;
     if(args.length != 0){
-      num = Integer.parseInt(args[0]);
+      int num = Integer.parseInt(args[0]);
       int result = 1;
-      // while(num>=1){
-      //   result = result * num;
-      //   num--;
-      // }
-      // for(int i=num;i>=1;i--){
-      //   result = result * num;
-      // }
-      do{
+      while(num>=1){
         result = result * num;
         num--;
-      }while(num>=1);
+      }
       System.out.println(result);
     }
   }
