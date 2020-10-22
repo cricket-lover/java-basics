cffe7dbfc23d3ddbc5f22e9fccf05c6d877bf2dc
Phaneendra <phaneendraandukuri@gmail.com>
2020-10-22 12:09:46 +0530

    added ConvertToCentigrade class which converts fahrenheit to centigrades

--- ConvertToFahrenheit.java
+++ ConvertToFahrenheit.java
@@ -2,6 +2,6 @@ public class ConvertToFahrenheit{
   public static void main(String[] args){
     int centigrade = Integer.parseInt(args[0]);
     double fahrenheit = (centigrade * 9/5.00) + 32;
-    System.out.println(fahrenheit);
+    System.out.println("Temperature of " + centigrade + " degrees centigrade in fahrenheit is " +  fahrenheit);
   }
 }
